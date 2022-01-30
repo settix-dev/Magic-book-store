244da0ec72abf40ead5697af9b591e2f6474f4fc
settix-dev <settimbalamech@gmail.com>
2022-01-30 15:25:55 +0300

    Update App.css

--- src/presentationalComponents/App.js
+++ src/presentationalComponents/App.js
@@ -1,17 +1,11 @@
 import React from "react";
-import BooksForm from "../containerComponents/BooksForm";
 import BooksList from "../containerComponents/BooksList";
 import "./App.css";
-import Footer from "./Footer";
 
 const App = () => {
   return (
     <div className="container-fluid px-5">
       <BooksList />
-      <div>
-        <BooksForm />
-      </div>
-      <Footer />
     </div>
   );
 };
