562b4c1b64e54d31796ba1b4579d3beb13888fed
settix-dev <settimbalamech@gmail.com>
2022-01-30 15:33:56 +0300

    Delete Header.js file

--- src/presentationalComponents/Header.js
+++
@@ -1,9 +0,0 @@
-const Header = () => {
-    return (
-        <header className="header">
-            <h1 className="title">Magic Books Inc</h1>
-        </header>
-    )
-}
-
-export default Header
